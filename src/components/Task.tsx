d733b63636c8ea2f99cc829036ae45817f5b17ae
fe-nunes <fernando.fnn97@gmail.com>
2023-02-20 19:15:19 -0300

    feat: create list features

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,20 +1,56 @@
-import { Circle, Trash } from "phosphor-react";
+import { Circle, Trash, CheckCircle } from "phosphor-react";
 import check from "../assets/check.svg";
 import styles from "./Task.module.css";
 
-export function Task() {
+interface TaskProps {
+  id: number;
+  content: string;
+  onDeleteTask: (content: number) => void;
+  onCompleteTask: (content: number) => void;
+  completed: boolean;
+}
+
+export function Task({
+  content,
+  id,
+  completed,
+  onDeleteTask,
+  onCompleteTask,
+}: TaskProps) {
+  function handleDeleteComment(id: number) {
+    onDeleteTask(id);
+  }
+
+  function handleCompleteTask(id: number) {
+    onCompleteTask(id);
+  }
+
   return (
     <div className={styles.taskContainer}>
-      <div className={styles.circle}>
-        <button>
-          <Circle size={20} />
-        </button>
+      <div>
+        {completed ? (
+          <div className={styles.circle}>
+            <button onClick={() => handleCompleteTask(id)}>
+              <Circle size={20} />
+            </button>
+          </div>
+        ) : (
+          <div className={styles.check}>
+            <button onClick={() => handleCompleteTask(id)}>
+              <CheckCircle color="#fff" size={20} />
+            </button>
+          </div>
+        )}
       </div>
-      <div className={styles.description}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      <div
+        className={`${
+          completed ? styles.description : styles.descriptionTaskCompleted
+        }`}
+      >
+        {content}
       </div>
       <div className={styles.trash}>
-        <button>
+        <button onClick={() => handleDeleteComment(id)}>
           <Trash size={20} />
         </button>
       </div>
