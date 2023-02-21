8f986d77584e5b7eb88cafe44821dce3175b79e1
fe-nunes <fernando.fnn97@gmail.com>
2023-02-20 21:33:11 -0300

    ref:  layout polishes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
 import styles from "./App.module.css";
 
-import { PlusCircle } from "phosphor-react";
 import { Task } from "./components/Task";
 import React from "react";
+import { TaskWrapperController } from "./components/TaskWrapperController";
+import { NotTask } from "./components/NotTask";
 
-interface TaskContentProps {
+export interface TaskContentProps {
   id: number;
   task_content: string;
   complete: boolean;
@@ -14,7 +15,7 @@ interface TaskContentProps {
 export function App() {
   const [tasks, setTasks] = React.useState<TaskContentProps[]>([]);
   const [newTaskText, setNewTaskText] = React.useState({
-    id: new Date().getTime(), //random number
+    id: new Date().getTime(),
     task_content: "",
     complete: false,
   });
@@ -73,47 +74,31 @@ export function App() {
           <Header />
         </section>
         <form onSubmit={handleCreateNewComment}>
-          <div className={styles.taskWrapper}>
-            <div className={styles.inputContainer}>
-              <input
-                type="text"
-                value={newTaskText.task_content}
-                placeholder="Adicione uma nova tarefa"
-                onChange={handleNewTaskContent}
-              />
-              <button type="submit" className={styles.button}>
-                <span>Criar</span>
-                <PlusCircle size={20} />
-              </button>
-            </div>
-            <div className={styles.dashboardWrapper}>
-              <div className={styles.blueColor}>
-                Tarefas Criadas:
-                <span className={styles.dashboardValue}>{tasks.length}</span>
-              </div>
-              <div className={styles.purpleColor}>
-                Concluídas:
-                <span className={styles.dashboardValue}>
-                  {howToTaskCompleted.length} de {tasks.length}
-                </span>
-              </div>
-            </div>
-          </div>
+          <TaskWrapperController
+            content={newTaskText.task_content}
+            amoutTask={howToTaskCompleted}
+            onNewTaskContent={handleNewTaskContent}
+            tasks={tasks}
+          />
         </form>
 
         <div className={styles.taskContainer}>
-          {tasks.map((task) => {
-            return (
-              <Task
-                id={task.id}
-                content={task.task_content}
-                key={task.id}
-                onDeleteTask={handleDeleteTask}
-                onCompleteTask={handleCompleteTask}
-                completed={!task.complete}
-              />
-            );
-          })}
+          {tasks.length === 0 ? (
+            <NotTask />
+          ) : (
+            tasks.map((task) => {
+              return (
+                <Task
+                  id={task.id}
+                  content={task.task_content}
+                  key={task.id}
+                  onDeleteTask={handleDeleteTask}
+                  onCompleteTask={handleCompleteTask}
+                  completed={!task.complete}
+                />
+              );
+            })
+          )}
         </div>
       </div>
     </main>
